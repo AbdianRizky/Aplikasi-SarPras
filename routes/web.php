6de848d236daaa4da22c791db579c70064079a6d
Adonisme2k <abdianrizky11@gmail.com>
2020-11-13 06:22:07 +0700

    Revert "membuat controller, routing, migration, model, dan backend scaffolding dengan laravel fortify"
    This reverts commit ca800af1a75627c0dbb3d1378f48d6e2f55a0ba3.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('/', [HomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
 });
